87be9521dc73d6c0ae19d431f5821d01c9a47729
aafrinabid <mohdaafrin@gmail.com>
2022-06-06 14:55:31 +0530

    added new nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch,Route ,Redirect} from 'react-router-dom';
+import Navbar from './components/NavBar/NavBar';
+import SignIn from './pages/AuthenticationPage';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+<Navbar />
+      
+      <Switch>
+        <Route path='/' exact>
+      <h1 className='text-green-500'>home page machane</h1>
+          
+        </Route>
+      <Route path='/login'>
+        <SignIn />
+      </Route>
+      <Route path='/sign-up'>
+        <SignUp />
+      </Route>
+      <Route path='*'>
+        <Redirect to='/'/>
+      </Route>
+      </Switch>
+
+      
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchBar from "../SearchBar";
+import Logo from "./Logo";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Menu from "./Menu";
+import NotificationsMenu from "./NotificationsMenu";
+
+const Navbar = () => {
+    const classes = useStyles();
+    const [navBackground, setNavBackground] = useState(false);
+
+    const changeBackground = () => setNavBackground(window.scrollY > 20);
+    window.addEventListener("scroll", changeBackground);
+
+    return (
+        <AppBar
+            position="fixed"
+            elevation={0}
+            className={classes.navbar }
+            component="nav"
+            style={{
+                backgroundColor: navBackground ? "rgb(20, 20, 20)" : "rgba(0, 0, 0, 0)",
+            }}
+        >
+            <Toolbar className='bg-slate-500'>
+                <Logo className={classes.logo} />
+                <Menu />
+                <SearchBar />
+                <NotificationsMenu />
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    navbar: (props) => ({
+        padding: theme.spacing(0, 3),
+        backgroundImage: "linear-gradient(to bottom,rgba(0,0,0,.7) 10%,rgba(0,0,0,0))",
+        transition: ".4s",
+    }),
+    logo: {
+        width: "90px",
+        marginRight: theme.spacing(2),
+    },
+}));
+
+export default Navbar;
