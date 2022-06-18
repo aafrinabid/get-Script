d659dddde2513a89ac8d9143d4ec9365a2b0090f
aafrinabid <mohdaafrin@gmail.com>
2022-06-18 14:11:39 +0530

    first page done going to do stepper form

--- src/components/Upload/UploadFormInput.js
+++ src/components/Upload/UploadFormInput.js
@@ -1,15 +1,57 @@
 import UploadForm from './UploadForm';
 import { Card, Select, TextField,CardContent,Grid, MenuItem} from '@mui/material'
+import { makeStyles } from "@material-ui/core/styles";
 import React,{useRef,useState} from 'react';
 import {Box} from '@mui/material';
 import classes from './UploadFormInput.module.css';
 import {FormControl,InputLabel,OutlinedInput} from '@mui/material';
 import { deepPurple } from '@mui/material/colors';
 
+
+
+const useStyles = makeStyles({
+    input: {
+      color: "white",
+      border:"2px solid white"
+    }
+  });
+
+
+const genres=['Action Genre',
+'Animation Genre',
+'Comedy Genre',
+    'Crime Genre',
+    'Drama Genre',
+    'Experimental Genre',
+    'Fantasy Genre',
+    'Historical Genre',
+    'Horror Genre',
+    'Romance Genre',
+    'Science Fiction Genre',
+    'Thriller Genre',
+    'Western Genre',
+    'Other Genres',]
+    
+    const url='https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80';
+    const divImage =  {
+    
+        backgroundImage: `linear-gradient(180deg, rgba(1, 3, 15, 0.952), rgba(73, 69, 68, 0.64)), url(${url})`,
+           height:'450px',
+        //    marginTop:'-70px',
+        //    fontSize:'50px',
+           backgroundSize: '120%',
+           backgroundRepeat: 'no-repeat',
+       };
+
 function UploadFormInput() {
-    const scriptTitle=useRef();
-    const [Entertainment,setEntertainment]=useState('');
+    const classes=useStyles();
+    const scriptTitle= useRef();
+    const [Entertainment,setEntertainment]= useState('');
+    const [genre,setGenre]=useState([]);
+
+    
     const [type,setType]=useState('');
+
 
     console.log(scriptTitle)
 
@@ -19,21 +61,33 @@ function UploadFormInput() {
     const handleChangeType=(e)=>{
         setType(e.target.value)
     }
+
+    const handleChangeGenre = (event) => {
+        const {
+          target: { value },
+        } = event;
+        setGenre(
+          // On autofill we get a stringified value.
+          typeof value === 'string' ? value.split(',') : value,
+        );
+      };
   return (
     <div className='flex justify-center'>
-  <Card className='w-3/4 '>
-    <CardContent>
-        <Grid container spacing={2}>
-            <Grid xs={12} item>
-                <TextField label="Title Name" placeholder='Enter Titile Name' variant='outlined' className='w-full' inputRef={scriptTitle} >
+  <Card className='w-1/2 text-white'style={divImage}>
+    <CardContent  >
+        <Grid container spacing={2} >
+            <Grid xs={12} item className='text-white'>
+              <InputLabel className='w-full text-white' id='demo-simple-select-label'>Script Title</InputLabel>
+
+                <TextField  placeholder='Enter Titile Name' inputProps={{ className: classes.input }}  variant='outlined' className='w-1/2 text-white' inputRef={scriptTitle} style={{color:'white'}} >
 
                 </TextField>
 
             </Grid>
             <Grid xs={12} sm={12}  item>
-              <InputLabel className='w-full' id='demo-simple-select-label'>Entertainment Type</InputLabel>
+              <InputLabel className='w-full text-white' id='demo-simple-select-label'>Entertainment Type</InputLabel>
               <Select
-              className='w-full mt-3 '
+              className='w-1/2 mt-3 text-white border-white border-2'
              labelId="demo-simple-select-label"
              id="demo-simple-select"
              value={Entertainment}
@@ -46,9 +100,9 @@ function UploadFormInput() {
                 
             </Grid>
             <Grid xs={12} sm={12}  item>
-              <InputLabel className='w-full' id='demo-simple-select-label'>Entertainment Type</InputLabel>
+              <InputLabel className='w-full text-white' id='demo-simple-select-label'>Entertainment Type</InputLabel>
               <Select
-              className='w-full mt-3 '
+              className='w-1/2 mt-3 text-white border-white border-2'
              labelId="demo-simple-select-label"
              id="demo-simple-select"
              value={type}
@@ -63,6 +117,26 @@ function UploadFormInput() {
 
 </Select>
                 
+            </Grid>
+            <Grid xs={12} sm={12}  item>
+            <InputLabel id="demo-multiple-name-label" className='w-full my-3 text-white'>Genres</InputLabel>
+        <Select
+        className='w-1/2 text-white border-white border-2'
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple
+          value={genre}
+          onChange={handleChangeGenre}
+        >
+          {genres.map((genre) => (
+            <MenuItem
+              key={genre}
+              value={genre}
+            >
+              {genre}
+            </MenuItem>
+          ))}
+        </Select>
             </Grid>
         </Grid>
     </CardContent>
