5e54f52dc5d440a20f46a6f6b8e2d2f6c2d0cc06
aafrinabid <mohdaafrin@gmail.com>
2022-06-21 13:19:07 +0530

    some chart added

--- src/components/Admin/Cards.js
+++ src/components/Admin/Cards.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React from 'react';
+import {Bar,Line} from 'react-chartjs-2';
+import { Card, Grid, Typography, CardContent } from '@material-ui/core';
+import Countup from 'react-countup';
+// let cx = classnames.bind(classes);
+import classes from './cards.module.css'
+import Chart from './chart/chart';
 
 function Cards() {
   return (
-    <div>Cards</div>
+        <div>
+		<div className={classes.container}>
+			<Grid container spacing={3} justify="center">
+				<Grid item component={Card} xs={12} md={2} >
+					<CardContent>
+						<Typography gutterBottom>Total users</Typography>
+						<Typography variant="h4" gutterBottom>
+							<Countup start={0} end={10000} duration={1.5} separator="," />
+						</Typography>
+						<Typography variant="body2" gutterBottom>
+							{/* {new Date(lastUpdate).toDateString()} */}
+						</Typography>
+						<Typography variant="body2" component="p">
+							Total number of users in the app.
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card} xs={12} md={2} >
+					<CardContent>
+						<Typography gutterBottom>New Accounts</Typography>
+						<Typography variant="h4" gutterBottom>
+							<Countup start={0} end={545} duration={2.5} separator="," />
+						</Typography>
+						<Typography variant="body2" gutterBottom>
+							{/* {new Date(lastUpdate).toDateString()} */}
+						</Typography>
+						<Typography variant="body2" component="p">
+							Number of users joined today.
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card} xs={12} md={2}>
+					<CardContent>
+						<Typography gutterBottom>Total Posts</Typography>
+						<Typography variant="h4" gutterBottom>
+							<Countup start={0} end={9000} duration={2.5} separator="," />
+						</Typography>
+						<Typography variant="body2" gutterBottom>
+							{/* {new Date(lastUpdate).toDateString()} */}
+						</Typography>
+						<Typography variant="body2" component="p">
+							number of posts posted by users
+						</Typography>
+					</CardContent>
+				</Grid>
+                <Grid item component={Card} xs={12} md={2} >
+					<CardContent>
+						<Typography gutterBottom>Total Posts Today</Typography>
+						<Typography variant="h4" gutterBottom>
+							<Countup start={0} end={600} duration={1.5} separator="," />
+						</Typography>
+						<Typography variant="body2" gutterBottom>
+							{/* {new Date(lastUpdate).toDateString()} */}
+						</Typography>
+						<Typography variant="body2" component="p">
+							Total number of Posts posted by users Today
+						</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</div>
+        <Chart />
+        </div>
   )
 }
 
---
+++ src/components/Admin/chart/chart.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+import classes from './chart.module.css';
+
+const Chart = () => {
+
+	const line = (
+		<Line
+			data={ {
+				labels: ['jan','feb','mar','apr','may','june','july'],
+				datasets: [{
+				  label: 'Script-Writer',
+				  data: [65, 59, 80, 81, 56, 55, 40],
+				  fill: false,
+				  borderColor: 'rgb(75, 192, 192)',
+				  tension: 0.1
+				},
+				{
+				  label: 'Producer',
+				  data: [105, 29, 83, 3, 36, 95, 4],
+				  fill: false,
+				  borderColor: 'rgb(75, 102, 192)',
+				  tension: 0.1
+				}]
+			  }} />
+				
+	);
+	return <div className={classes.container}> {line}</div>;
+};
+
+export default Chart;
