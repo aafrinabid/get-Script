4fb9c82ebbd77e65dcf52cd6917f5395f4390e37
aafrinabid <mohdaafrin@gmail.com>
2022-06-29 19:41:50 +0530

    admin panel done for now

--- src/components/Admin/NavBar.js
+++ src/components/Admin/NavBar.js
@@ -25,7 +25,8 @@ import ListItemText from '@mui/material/ListItemText';
 import Check from '@mui/icons-material/Check';
 import { authActions } from '../../assets/store/authSlice';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -67,6 +68,18 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function PrimarySearchAppBar() {
+  const [notDetails,setNotDetails]=useState([])
+  const [isLoading,setIsLoading]=useState(false)
+  useEffect(()=>{
+  setIsLoading(true)
+   axios.get('http://localhost:4000/fetchnotification').then((res)=>{
+    setNotDetails([...res.data.result])
+    setIsLoading(false)
+   }).catch(e=>{console.log(e)
+  setIsLoading(false)
+  })
+  },[])
+  console.log(notDetails)
   const dispatch= useDispatch();
   const optionShower=()=>dispatch(UiAction.onClickLogo())
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -168,6 +181,7 @@ export default function PrimarySearchAppBar() {
           {/* </Badge> */}
         </IconButton>
         <Menu
+                // className='h-full'
                 id="notifications"
                 anchorEl={notAnchorEl}
                 anchorOrigin={{
@@ -185,41 +199,22 @@ export default function PrimarySearchAppBar() {
                 {/* <MenuItem className='bg-black' onClick={notHandleClose}>Nolan requested for Producer Access </MenuItem>
                 <MenuItem onClick={notHandleClose}>Rafi requested for Producer Access</MenuItem>
                 <MenuItem onClick={notHandleClose}>Kamal requested for Producer Access </MenuItem> */}
-                 <Paper sx={{ width: 320,backgroundColor:'black',color:'white' ,height:240}}>
-      <MenuList dense>
-        <MenuItem>
-          <ListItemText >
-          <Typography variant="inherit" noWrap>Rafi tharoor babybabu requested for Producer Access</Typography></ListItemText>
-        </MenuItem>
+                 <Paper sx={{ width: 320,color:'black' ,height:'100%'}}>
+      <MenuList >
+        {isLoading && <h1>Loading...</h1>}
+        {!isLoading && notDetails.map(detail=>(
+          <>
+           <MenuItem onClick={notHandleClose}>
+           <ListItemText >
+             <Link to='/AdminPanel/Requests' onClick={notHandleClose}> <Typography variant='inherit' noWrap >{detail.username} requested for Producer Access</Typography></Link>
+           </ListItemText>
+           </MenuItem>
         <Divider/>
-        <MenuItem>
-          <ListItemText >molan requested for Producer Access</ListItemText>
-        </MenuItem>
-        <Divider/>
-        <MenuItem>
-          <ListItemText >saucer requested for Producer Access</ListItemText>
-        </MenuItem>
-        <Divider/>
-        <MenuItem>
-          <ListItemText >
-           best andersom e requested for Producer Access
-          </ListItemText>
-        </MenuItem>
-           <Divider/>
-        {/* <Divider /> */}
-        <MenuItem>
-          <ListItemText >lul jasi requested for Producer Access</ListItemText>
-        </MenuItem>
-        <Divider/>
-        <MenuItem>
-          <ListItemText >jojar requested for Producer Access</ListItemText>
-        </MenuItem>
-        <Divider/>
-        {/* <Divider /> */}
-        <MenuItem>
-          <ListItemText >Rafi requested for Producer Access</ListItemText>
-        </MenuItem>
-        <Divider/>
+        </>
+
+
+        ))}
+       
       </MenuList>
     </Paper>
 
@@ -310,7 +305,7 @@ export default function PrimarySearchAppBar() {
               color="inherit"
               onClick={notHandleClick}
             >
-              <Badge badgeContent={17} color="error">
+              <Badge badgeContent={notDetails.length} color="error">
                 <NotificationsIcon />
               </Badge>
             </IconButton>
