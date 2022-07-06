689ca496fb8b1a7b92d9d5cd2232f86cef95c6e6
aafrinabid <mohdaafrin@gmail.com>
2022-07-06 17:40:06 +0530

    script details fetched for now more to do

--- src/components/Details/ScriptCard.js
+++ src/components/Details/ScriptCard.js
@@ -6,21 +6,21 @@ import {AddBoxRounded} from '@mui/icons-material'
 // import {FavoriteIcon} from '@mui/icons-material'
 import { DoneRounded } from '@material-ui/icons';
 
-function ScriptCard() {
+function ScriptCard(props) {
 
-    let url="https://cdn.mos.cms.futurecdn.net/9Y8SXNLpFF6ENYKuuWrQ7K.jpg"
+    let url=props.img
     const divImage =  {
         backgroundImage: `url(${url})`,
-           height:'500px',
+           height:'400px',
         //    marginTop:'-70px',
         //    fontSize:'50px',
-        //    backgroundSize: '100%',
+           backgroundSize: '100%',
            backgroundRepeat: 'no-repeat',
        };
   return (
       <div className={`${classes.border}`} style={divImage}>
         <div className={classes.scriptcard} >
-        <img className={classes.titleimage} src="https://occ-0-6058-3663.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUJuexBo9PcuDem99SxCIRkLb6vllV0EfehwnalimUa6Wqqm8gSFD8zkIjAUzPt4byng_57yB8mtGQQOkeE7tXaj6vT7kcZPjWYLARDnTtWQ0BpJg-XYmiEXRN1IcSPFw4iAb9b9vIDg9ggS79aLU8shH8gk_YsyLIAnQfKPgyreG6t667uzow.png?r=e43" />
+        {/* <img className={classes.titleimage} src="https://occ-0-6058-3663.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUJuexBo9PcuDem99SxCIRkLb6vllV0EfehwnalimUa6Wqqm8gSFD8zkIjAUzPt4byng_57yB8mtGQQOkeE7tXaj6vT7kcZPjWYLARDnTtWQ0BpJg-XYmiEXRN1IcSPFw4iAb9b9vIDg9ggS79aLU8shH8gk_YsyLIAnQfKPgyreG6t667uzow.png?r=e43" /> */}
         {/* <div className={classes.button}>
         <Button  variant='contained' id={classes.download} size='large'>Download</Button>
          
--- src/components/Details/ScriptDetail.js
+++ src/components/Details/ScriptDetail.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import ScriptCard from './ScriptCard';
 import classes from './ScriptDetail.module.css'
 import SuggestionRows from '../SuggestionRows/SuggestionRows'
@@ -7,9 +7,33 @@ import ScriptTable from './ScriptTable';
 import ScriptPdf from './ScriptPdf'
 import Rows from '../Rows/Rows';
 import Button from '@mui/material/Button';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 function ScriptDetail() {
+    const params=useParams()
+    const [detail,setDetail]=useState([]);
+    console.log(detail)
+    const [isLoading,setIsLoading]=useState(false)
+    const {scriptId}=params
+    console.log(scriptId)
+    useEffect(()=>{
+       setIsLoading(true)
+        axios.get('http://localhost:4000/scriptdetails',{
+            headers:{
+                'scriptid':scriptId
+            }
+        }).then((res)=>{
+            setDetail(res.data.result)
+            setIsLoading(false)
+       
+        }).catch(e=>{
+            setIsLoading(false)
+
+        })
+
+    },[])
     const [seenTable,setSeenTable]=useState(false);
     const [seenScript,setSeenScript]=useState(false);
 
@@ -23,8 +47,11 @@ function ScriptDetail() {
 
   return (
     <div className={classes.scriptdetails}>
-        <ScriptCard />
-        <ScriptInfo />
+        {isLoading && <h1>loading....</h1>}
+        {!isLoading &&  
+        <>
+        <ScriptCard img={detail.script_poster}/>
+        <ScriptInfo detail={detail}/>
         <div className={classes.tablediv}>
            {!seenTable && <Button variant='text' className='text-4xl font-bold text-white p-7' onClick={clickHandler}>Show Pitch</Button>} 
         {seenTable &&<ScriptTable clickHandler={clickHandler}/>}
@@ -37,6 +64,8 @@ function ScriptDetail() {
             <h1 className='text-xl text-white p-5'>You may also like these scripts...</h1>
             <Rows screenper='4' />
         </div>
+        </>
+        }
         </div>
 
 
