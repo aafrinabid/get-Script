fdcc6871079987ce9271a432a12157a663a2af6f
aafrinabid <mohdaafrin@gmail.com>
2022-07-06 13:56:36 +0530

    rows added

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
 import Rows from '../components/Rows/Rows';
+const genres=['Action Genre',
+'Animation Genre',
+'Comedy Genre',
+    'Crime Genre',
+    'Drama Genre',
+    'Experimental Genre',
+    'Fantasy Genre',
+    'Historical Genre',
+    'Horror Genre',
+    'Romance Genre',
+    'Science Fiction Genre',
+    'Thriller Genre',
+    'Western Genre',
+    'Other Genres',]
 
 function Browse() {
   return (
     <div className='bg-black'>
         <Banner  />
+        <Rows screenper={6} genre='Science Fiction Genre' />
+        <Rows screenper={6} genre='Comedy Genre' />
+
+        {/* <Rows screenper={6} />
         <Rows screenper={6}/>
         <Rows screenper={6}/>
         <Rows screenper={6}/>
-        <Rows screenper={6}/>
-        <Rows screenper={6}/>
-        <Rows screenper={6}/>
+        <Rows screenper={6}/> */}
 
     </div>
   )
