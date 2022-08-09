3fcb9c1d87e17ffe79622c9b896932aec3e58a08
aafrinabid <mohdaafrin@gmail.com>
2022-08-09 15:40:54 +0530

    border color added

--- src/components/Rows/Rows.js
+++ src/components/Rows/Rows.js
@@ -9,6 +9,7 @@ import 'swiper/css/scrollbar';
 import {useHistory} from 'react-router-dom'
 import { LineAxisSharp } from "@mui/icons-material";
 import axios from "axios";
+import { borderColor } from "@mui/system";
 
 const genres=['Action Genre',
 'Animation Genre',
@@ -32,16 +33,35 @@ function Rows(props) {
   const [scripts,setScripts]=useState([])
   const [genre,setGenre]=useState('')
   // console.log(scripts)
-  const backgroundChanger=(url)=>{
-    const divImage =  {
+  const backgroundChanger=(url,featured)=>{
+    console.log(featured)
+    let divImage ={}
+    if(!featured){
+      divImage={
     
-      backgroundImage: `linear-gradient(180deg, rgba(1, 3, 15, 0.252), rgba(73, 69, 68, 0.64)), url(${url})`,
-         height:'110px',
-      //    marginTop:'-70px',
-      //    fontSize:'50px',
-         backgroundSize: '100%',
-         backgroundRepeat: 'no-repeat',
-     };
+        backgroundImage: `linear-gradient(180deg, rgba(1, 3, 15, 0.252), rgba(73, 69, 68, 0.64)), url(${url})`,
+           height:'110px',
+        //    marginTop:'-70px',
+        //    fontSize:'50px',
+           backgroundSize: '100%',
+           backgroundRepeat: 'no-repeat',
+           
+       };
+    }  else{
+      divImage={
+    
+        backgroundImage: `linear-gradient(180deg, rgba(1, 3, 15, 0.252), rgba(73, 69, 68, 0.64)), url(${url})`,
+           height:'110px',
+        //    marginTop:'-70px',
+        //    fontSize:'50px',
+           backgroundSize: '100%',
+           backgroundRepeat: 'no-repeat',
+          //  borderRadius:'1px',
+          //  borderColor:'red'
+          border:'1px solid red'
+       };
+
+    }
      return divImage
   }
 
@@ -95,9 +115,17 @@ setIsLoading(false)
 >
   {isLoading && <h1>loading ...</h1>}
   {!isLoading && scripts.map(script=>(
- <SwiperSlide><div className={classes.poster}style={backgroundChanger(script.script_poster)} onClick={details.bind(null,script.script_id)}>{
-  script.featured?<h3 style={{color:'red',textAlign:'end'}}>FEATURED</h3>:''
- } <h1 className="text-white">{script.script_title}</h1></div> </SwiperSlide>
+ <SwiperSlide>
+  {/* <div style={{borderRadius:'2px',borderColor:'red'}}> */}
+  <div className={classes.poster}style={backgroundChanger(script.script_poster,script.featured)} onClick={details.bind(null,script.script_id)}>
+  <div style={{display:'grid',gridTemplateColumns:'1fr',gridTemplateRows:'2fr 1fr'}}>
+ <h1 className="text-white">{script.script_title}</h1>
+
+ {/* </div> */}
+ </div> 
+ </div>
+ {/* </div> */}
+  </SwiperSlide>
 //  <SwiperSlide><div className={classes.poster}  onClick={details.bind(null,script.script_id)}> </div> </SwiperSlide>
 ))}
     {/* <SwiperSlide><div className={classes.poster} onClick={details}> <img className={classes.imageTitle} src="https://occ-0-6058-3663.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUJuexBo9PcuDem99SxCIRkLb6vllV0EfehwnalimUa6Wqqm8gSFD8zkIjAUzPt4byng_57yB8mtGQQOkeE7tXaj6vT7kcZPjWYLARDnTtWQ0BpJg-XYmiEXRN1IcSPFw4iAb9b9vIDg9ggS79aLU8shH8gk_YsyLIAnQfKPgyreG6t667uzow.png?r=e43"></img></div> </SwiperSlide>
