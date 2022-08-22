0fc222a4aa7098ae45963eba940b0a6fa4adeb35
aafrinabid <mohdaafrin@gmail.com>
2022-08-22 13:55:36 +0530

    design almost done going to make query

--- src/components/PostedScriptCards/ScriptPostedCards.js
+++ src/components/PostedScriptCards/ScriptPostedCards.js
@@ -8,6 +8,31 @@ import Typography from '@mui/material/Typography';
 import { useEffect } from 'react';
 import classes from './ScriptPostedCards.module.css';
 import axios from 'axios';
+const backgroundChanger=(url)=>{
+  
+    let divImage={
+  
+      // backgroundImage: `linear-gradient(180deg, rgba(1, 3, 15, 0.252), rgba(73, 69, 68, 0.64)), url(${url})`,
+        backgroundImage:`url(${url})`,
+         height:'300px',
+         position:'relative',
+         cursor:'pointer',
+         width:'100%',
+         color: "white",
+      //    marginTop:'-70px',
+      //    fontSize:'50px',
+         padding:'142px',
+         backgroundSize: 'cover',
+         backgroundRepeat: 'no-repeat',
+         cursor:'pointer',
+        //  backgroundSize: '111%'
+         
+     };
+    
+
+  
+   return divImage
+}
 
 function ScriptPostedCards(props) {
   useEffect(()=>{
@@ -27,31 +52,42 @@ function ScriptPostedCards(props) {
 
     }
   return (
-    <div className={`${classes.postedscripts}  bg-inherit`}>
+    <div className={classes.gridWrapper}>
+    <div className={classes.postedscripts}>
         {scripts.map((i)=>(
-             <Card key={i} sx={{ maxWidth: 345 }}    className= {`bg-black text-white ${classes.scriptCard}`}  >
-             <CardMedia
-             className={`${classes.image}`}
-               component="img"
-               height="340"
-               image="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-               alt="green iguana"
-             />
-             <Typography gutterBottom variant="h5" component="div">
-                 Lizard
-               </Typography>
-             <CardContent className= { `${classes.description}`} >
-               
-               <Typography variant="body2" color="text.secondary" className='text-white'>
-                 Lizards are a widespread group of squamate reptiles, with over 6,000
-                 species, ranging across all continents except Antarctica
-               </Typography>
-             </CardContent>
-           </Card>
+          <div key={i} className={classes.background}   style={backgroundChanger('https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')}>
+      {/* <div className={classes.background}> */}
+            <div className={classes.DisplayOver}>
+              <h1 className={classes.BigTitle}>
+                Tinder
+              </h1>
+            <div className={classes.hover}>
+             <h4 className={classes.subTitle}>
+               noce
+             </h4> 
+            <p className={classes.paragraph}>
+              MOre description herererer
+
+            </p>
+              
+            </div>
+
+            </div>
+          {/* </div> */}
+          </div>
+ 
         ))}
         
+    </div>
     </div>
   )
 }
 
 export default ScriptPostedCards
+
+
+
+
+
+
+
