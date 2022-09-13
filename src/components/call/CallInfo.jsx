b1b2f263c1c43c4f0babc210d2c331001f9eebf8
aafrinabid <mohdaafrin@gmail.com>
2022-09-13 15:50:59 +0530

    style chages

--- src/components/call/CallInfo.jsx
+++ src/components/call/CallInfo.jsx
@@ -5,9 +5,9 @@ function CallInfo() {
   const {call,answerCall,leaveCall}=useContext(SocketContext)
   return (
     <div className='flex flex-col'>
-      <h1>{call.name} is calling you</h1>
+      <h1>{call.name} <br/>is calling you</h1>
       <div>
-      <Button style={{color:'white',background:'green', margin:'20px'}} onClick={answerCall}>answer</Button> 
+      <Button style={{color:'white',background:'green', margin:'20px',marginLeft:'2px'}} onClick={answerCall}>answer</Button> 
       <Button style={{color:'white',background:'red'}} onClick={leaveCall}>reject</Button>
       </div>
     </div>
